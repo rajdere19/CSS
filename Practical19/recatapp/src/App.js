e2fa7290d010b4c615c06014ce1675afac899e45
rajdere19 <rajdere19@gmail.com>
2026-04-08 13:33:03 +0000

    hii

---
+++ Practical19/recatapp/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1 className="heading">Welcome to React</h1>
+      <p className="para">This is a styled paragraph.</p>
+      <button className="btn">Click Here</button>
+    </div>
+  );
+}
+
+export default App;
